3ca965677442ee94293c0aa97092b9f93723da58
nandotiamat <tambu00matt@gmail.com>
2026-02-04 19:41:04 +0100

    Downgrade flavor to m1.nano to fix resource exhaustion

--- app-demo/openstack/variables.tf
+++ app-demo/openstack/variables.tf
@@ -5,7 +5,7 @@ variable "image_name" {
 
 variable "flavor_name" {
   type    = string
-  default = "m1.small"
+  default = "m1.nano" # Reduced from m1.small to fit in DevStack memory constraints
 }
 
 variable "db_password" {
