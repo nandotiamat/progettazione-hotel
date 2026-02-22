71953256b86d2c1977cc5d147c15f86f554f9085
nandotiamat <tambu00matt@gmail.com>
2026-02-22 14:47:37 +0100

    remove backend fip output, remove keys folder

--- app-demo/openstack/reference/output.tf
+++ app-demo/openstack/reference/output.tf
@@ -13,10 +13,10 @@ output "db_private_ip" {
   value       = openstack_compute_instance_v2.db.access_ip_v4
 }
 
-output "backend_public_ip" {
-  description = "Il Floating IP per collegarti via browser o SSH"
-  value       = openstack_networking_floatingip_v2.backend_fip.address
-}
+# output "backend_public_ip" {
+#   description = "Il Floating IP per collegarti via browser o SSH"
+#   value       = openstack_networking_floatingip_v2.backend_fip.address
+# }
 
 output "swift_media_container_name" {
   description = "Il nome del container Swift per i media"
